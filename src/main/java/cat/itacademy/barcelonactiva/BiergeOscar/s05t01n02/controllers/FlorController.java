fc0615373e69d9141f35082f4bfcd7c5e9446c86
Oscar <okeshik@protonmail.com>
2024-03-21 10:31:54 +0100

    afegeixo descripcio als endpoints

--- src/main/java/cat/itacademy/barcelonactiva/BiergeOscar/s05t01n02/controllers/FlorController.java
+++ src/main/java/cat/itacademy/barcelonactiva/BiergeOscar/s05t01n02/controllers/FlorController.java
@@ -2,7 +2,9 @@ package cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.controllers;
 
 import cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.model.dto.FlorDTO;
 import cat.itacademy.barcelonactiva.BiergeOscar.s05t01n02.model.services.FlorService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flor")
+@Tag(name = "Programa de les Flors ", description = "Operacions pertinents a les Flors")
 public class FlorController {
 
     @Autowired
@@ -20,6 +23,7 @@ public class FlorController {
 
     //add
     @GetMapping("/add")
+    @Operation(summary = "Afegeix una nova Flor", description = "Aquesta API permet afegir una nova Flor")
     public ResponseEntity<FlorDTO> addFlor (FlorDTO florDTO){
         FlorDTO florDTO1 = florService.add(florDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(florDTO1);
@@ -28,6 +32,7 @@ public class FlorController {
 
     //update
     @PutMapping("/update/{id}")
+    @Operation(summary = "Actualitza una flor amb el seu ID", description = "Aquesta API permet actualitzar una flor existent")
     public ResponseEntity<FlorDTO> updateFlor (@PathVariable Long id, @RequestBody FlorDTO florDTO){
         try {
             FlorDTO florDTO1 = florService.update(id, florDTO);
@@ -40,6 +45,7 @@ public class FlorController {
 
     //deleteById
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Elimina una Flor per ID", description = "Aquesta API permet eliminar una Flor existent")
     public ResponseEntity<String> deleteFlor(@PathVariable Long id){
         try{
             String mensaje = florService.deleteById(id);
@@ -53,6 +59,7 @@ public class FlorController {
 
     //getOne
     @GetMapping("/getOne/{id}")
+    @Operation(summary = "Consulta una Flor per ID", description = "Aquesta API permet consultar les dades de una Flor existent")
     public ResponseEntity<FlorDTO> getOneFlor (@PathVariable Long id){
         try {
             FlorDTO florDTO = florService.findById(id);
@@ -65,6 +72,7 @@ public class FlorController {
 
     //getAll
     @GetMapping("/getAll")
+    @Operation(summary = "Consulta totes les Flors", description = "Aquesta API permet consultar el llistat de totes les Flors exitents")
     public ResponseEntity<List<FlorDTO>> getAllFlors(){
         List<FlorDTO> flores = florService.findAll();
 
